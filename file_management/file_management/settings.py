1b3eed7c378815be2fdb184fbd3f67193849cfc2
BlacklabeI <johnkibe56@gmail.com>
2022-02-28 16:23:42 +0300

    Fixed user registration using Django Signals

--- file_management/file_management/settings.py
+++ file_management/file_management/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'accounts',
     'django_filters',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
